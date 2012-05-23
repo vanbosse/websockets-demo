fd72e30c2c84f31aa80337802736bc5bd6d381de
Jeroen Van den Bossche <vanbosse.jeroen@gmail.com>
2012-05-23 21:47:01 +0200

    Add server and client.

---
+++ js/client.js
@@ -0,0 +1,45 @@
+/**
+ * The client!
+ *
+ * @author Jeroen Van den Bossche <jeroen@vanbosse.be>
+ */
+
+var socket = io.connect('http://localhost:8080');
+var nick = window.prompt('Nickname:');
+var form = document.querySelector('#send');
+var input = document.querySelector('#message');
+var messages = document.querySelector('.messages');
+var users = document.querySelector('.users');
+
+// send nickname to server.
+socket.emit('register nickname', nick);
+
+// register handler to receive messages.
+socket.on('message', function(message) {
+	var output = '<p>&lt;' + message.nickname + '&gt; ' + message.message;
+	messages.innerHTML += output;
+});
+
+// register a handler to receive new connected users.
+socket.on('user connected', function(nickname) {
+	messages.innerHTML += '<p class="green">' + nickname + ' is connected.</p>';
+	users.innerHTML += '<p>' + nickname + '</p>';
+});
+
+// register a handler to receive disconnected users.
+socket.on('user disconnected', function(nickname) {
+	messages.innerHTML += '<p class="red">' + nickname + ' is disconnected.</p>';
+});
+
+// register the handler to receive all connected users.
+socket.on('user', function(nickname) {
+	users.innerHTML += '<p>' + nickname + '</p>';
+});
+
+// register handler for submission of the form.
+form.onsubmit = function(evt)
+{
+	evt.preventDefault();
+	socket.emit('message', input.value);
+	input.value = '';
+}
---
+++ js/server.js
@@ -0,0 +1,44 @@
+/**
+ * The server!
+ *
+ * @author Jeroen Van den Bossche <jeroen@vanbosse.be>
+ */
+
+var io = require('socket.io').listen(8080);
+var users = [];
+
+// a socket got connected.
+io.sockets.on('connection', function(socket) {
+
+	// when a socket connects,
+	// send them all connected users.
+	users.forEach(function(nickname) {
+		socket.emit('user', nickname);
+	});
+
+	// when a socket is connected and sends the "register nickname" event,
+	// we set the nickname to the socket
+	// and send a connected message to all sockets.
+	socket.on('register nickname', function(nickname) {
+		socket.set('nickname', nickname, function() {
+			users.push(nickname);
+			io.sockets.emit('user connected', nickname);
+		});
+	});
+
+	// when we receive a message from one of the connected sockets,
+	// get its nickname and send the message to all connected sockets.
+	socket.on('message', function(message) {
+		socket.get('nickname', function(error, nickname) {
+			io.sockets.emit('message', {nickname: nickname, message: message});
+		});
+	});
+
+	// when a socket disconnects,
+	// send a message to alert other sockets.
+	socket.on('disconnect', function() {
+		socket.get('nickname', function(error, nickname) {
+			io.sockets.emit('user disconnected', nickname);
+		});
+	});
+});
