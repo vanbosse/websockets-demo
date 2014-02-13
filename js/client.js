bf2aa22a6586740fdd18607e0d49985db5cdb3a0
Jeroen Van den Bossche <vanbosse.jeroen@gmail.com>
2014-02-13 18:11:21 +0100

    Client: add time for chat.

--- js/client.js
+++ js/client.js
@@ -18,19 +18,19 @@ window.onload = function()
 
 	// register handler to receive messages.
 	socket.on('message', function(message) {
-		var output = '<p>&lt;' + message.nickname + '&gt; ' + message.message;
+		var output = '<p>' + getTime() + ' &lt;' + message.nickname + '&gt; ' + message.message;
 		messages.innerHTML += output;
 	});
 
 	// register a handler to receive new connected users.
 	socket.on('user connected', function(nickname) {
-		messages.innerHTML += '<p class="green">' + nickname + ' is connected.</p>';
+		messages.innerHTML += '<p class="green">' + getTime() + ' ' + nickname + ' is connected.</p>';
 		users.innerHTML += '<p id="' + nickname + '">' + nickname + '</p>';
 	});
 
 	// register a handler to receive disconnected users.
 	socket.on('user disconnected', function(nickname) {
-		messages.innerHTML += '<p class="red">' + nickname + ' is disconnected.</p>';
+		messages.innerHTML += '<p class="red">' + getTime() + ' ' + nickname + ' is disconnected.</p>';
 		users.removeChild(document.querySelector('#' + nickname));
 	});
 
@@ -38,6 +38,11 @@ window.onload = function()
 	socket.on('user', function(nickname) {
 		users.innerHTML += '<p id="' + nickname + '">' + nickname + '</p>';
 	});
+
+    getTime = function() {
+        var date = new Date();
+        return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+    }
 
 	// register handler for submission of the form.
 	form.onsubmit = function(evt)
